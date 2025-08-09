b577ee655021fdf7558867294b78d971ea79d29f
Asadulla Khatami <asadullakhatami@gmail.com>
2025-08-09 22:01:05 +0600

    functionality is added and game is work with some problem i will fixed it

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ let playerX = true;
 let trackMove = 0;
 
 const winPatterns = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8]
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
 ];
 
 let setPlayer = () => {
     let num = Math.floor(Math.random() * 9);
     // console.log(num);
-    if (num % 2 == 0) {
+    if(num % 2 == 0){
         playerX = true;
         // console.log(playerX);
-    } else {
+    }else{
         playerX = false;
         // console.log(playerX);
 
@@ -37,13 +37,13 @@ const resetGame = () => {
     enabledBtn();
 }
 
-for (let box of boxes) {
+for(let box of boxes){
     box.addEventListener("click", () => {
         // console.log("button was clicked");
-        if (playerX) {
+        if(playerX){
             box.innerText = 'X';
             playerX = false;
-        } else {
+        }else{
             box.innerText = 'O';
             playerX = true;
         }
@@ -72,35 +72,35 @@ let drawMess = () => {
 }
 
 const checkWiner = (boxes) => {
-    for (let winPattern of winPatterns) {
+    for(let winPattern of winPatterns){
 
-        let move1 = boxes[winPattern[0]].innerText;
-        let move2 = boxes[winPattern[1]].innerText;
-        let move3 = boxes[winPattern[2]].innerText;
+       let move1 = boxes[winPattern[0]].innerText;
+       let move2 = boxes[winPattern[1]].innerText;
+       let move3 = boxes[winPattern[2]].innerText;
 
-        if (move1 !== "" && move2 !== "" && move3 !== "") {
-            if (move1 === move2 && move2 === move3) {
-                console.log(`winner is ${move1}`);
-                shoWinner(move1);
-                disBtn();
-                trackMove = 0;
-            }
+       if(move1 !== "" && move2 !== "" && move3 !== ""){
+        if(move1 === move2 && move2 === move3){
+            console.log(`winner is ${move1}`);
+            shoWinner(move1);
+            disBtn();
+            trackMove = 0;
         }
-
-        if (trackMove === 9) {
+       }
+       
+       if(trackMove === 9){
             drawMess();
-        }
+       }
     }
 }
 
-function disBtn() {
-    for (let box of boxes) {
+function disBtn(){
+    for(let box of boxes){
         box.disabled = true;
     }
 };
 
 function enabledBtn() {
-    for (let box of boxes) {
+    for(let box of boxes){
         box.disabled = false;
         box.innerText = "";
     }
